9a94d3b5c1012aa1655fd4233591645a348f8781
Yuval-Dev <yuvalvaknin2004@gmail.com>
2020-06-23 15:26:28 -0400

    quick change

--- compile.sh
+++ compile.sh
@@ -3,8 +3,8 @@
 #ion of the data that should be on the boot device.
 
 CROSS_COMPILER_PATH="$HOME/opt/cross/bin" #edit as you see fit
-NASM_COMPILER_PATH="$HOME/opt/cross/bin:$PATH" #edit as you see fit
-QEMU_EXECUTABLE_PATH="$HOME/opt/cross/bin:$PATH" #edit as you see fit
+NASM_COMPILER_PATH="$HOME/opt/cross/bin" #edit as you see fit
+QEMU_EXECUTABLE_PATH="$HOME/opt/cross/bin" #edit as you see fit
 #^^^ I didn't edit any of these because if you use arch/manjaro and use pacman
 #to install the 'qemu', 'nasm' and 'i686-elf-gcc' packages they are already in
 #the right place. If you do not have that option available, follow this:
@@ -12,12 +12,14 @@ QEMU_EXECUTABLE_PATH="$HOME/opt/cross/bin:$PATH" #edit as you see fit
 #compiler and as for installing nasm and qemu, it is really easy.
 
 export PATH="$CROSS_COMPILER_PATH:$PATH" #this should be fine as is
+export PATH="$NASM_COMPILER_PATH:$PATH"
+export PATH="$QEMU_EXECUTABLE_PATH:$PATH"
 export TARGET="i686-elf" #edit as you see fit
 
 nasm -f elf ./asm/*.asm #to compile all the assembly files
 #^^^ compile the assembly files
 
-$CROSS_COMPILER_PATH/$TARGET-gcc *.cpp ./asm/*.o -o kernel.bin -nostdlib -ffree\
+$TARGET-gcc *.cpp ./asm/*.o -o kernel.bin -nostdlib -ffree\
 standing -std=c++11 -mno-red-zone -fno-exceptions -nostdlib -fno-rtti -Wall -We\
 xtra -Werror -T linker.ld -I./include/
 #^^^ compile the c, cpp and o files from the assembly stage and link them using
